9d8e3b34ea9929fd2b524c2e54587c53a7944183
Prateekk02 <prateek.kumar2002@gmail.com>
2025-02-09 15:51:16 +0530

    Prisma config and model creation

---
+++ app/api/videos/route.ts
@@ -0,0 +1,18 @@
+import {prisma} from "@/lib/prisma"
+import { NextRequest,NextResponse } from "next/server"
+
+export async function GET(request:NextRequest){
+    try{
+        const videos = await prisma.video.findMany({
+            orderBy: {createdAt: 'desc'}
+        })
+
+        return NextResponse.json(videos)
+    }catch(error){
+        console.log(error)
+        return NextResponse.json({
+            message: "Error fetching videos",
+            status: 500 
+        })
+    }
+}
