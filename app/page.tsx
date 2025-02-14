c3903840ab9fbd9571b222f49a568aa5041600ee
Prateekk02 <prateek.kumar2002@gmail.com>
2025-02-14 18:43:43 +0530

    Fixed route issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,17 @@
 'use client'
-import React,{useState, useCallback, useEffect} from 'react'
-import axios from 'axios'
-import VideoCard from '@/components/VideoCard'
-import { Video } from '@/types'
-import { VideoCardSkeleton } from '@/components/VideoCardSkeleton'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 function Home() {
-  const [videos, setVideos] = useState<Video[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error,setError] = useState<string | null>(null);
-
-  const fetchVideo = useCallback(async () =>{
-    try{
-      const response = await axios.get("/api/videos")
-
-      if(Array.isArray(response.data)){
-        setVideos(response.data)
-      }else{
-        throw new Error("Unexpected response format.")
-      }
-    }catch(e){
-      setError("Failed to fetch video")
-      console.log(`Failed to load videos: ${e} :: ${error} `,)
-
-    }finally{
-      setLoading(false);
-    }
-  },[error])
+  const router = useRouter();
 
   useEffect(() => {
-    fetchVideo()
-  }, [fetchVideo])
 
-  const handleDownload = useCallback((url:string, title:string) =>{
-    const link = document.createElement('a')
-    link.href = url;
-    link.setAttribute("download",`${title}.mp4`);
-    link.setAttribute("target","_blank");
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link)
-  },[])
+    router.push("/home");
+  }, [router]);
 
-  if(loading){
-    return <VideoCardSkeleton/>
-  }
-  return (
-    <div className='container mx-auto p-4'>
-      <h1 className="text-2xl font-bold mb-4">Videos</h1>
-      {videos.length === 0 ? (<div className="text-center text-lg text-gray-500">No videos available.</div>) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {videos.map((video) => (
-              <VideoCard
-                key={video.id}
-                video={video}
-                onDownload={handleDownload}
-              /> 
 
-            ))}
-          </div>
-      ) }
-
-    </div>
-  )
+  return null;
 }
 
-export default Home
+export default Home;
